ba9220b0580d40658d774ae24ff05c90e727563e
xxsoxx0621 <45780936+xxsoxx0621@users.noreply.github.com>
2021-07-09 22:22:54 +0900

    gugudan

---
+++ eclipse-workspace/HelloWorld/src/gugudan.java
@@ -0,0 +1,32 @@
+
+public class gugudan {
+	public static void main(String[] arg) {
+		//2단
+		System.out.println("2단");
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2*4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*7);
+		System.out.println(2*8);
+		System.out.println(2*9);
+		//3단
+		System.out.println("3단");
+		System.out.println(3*1);
+		System.out.println(3*2);
+		System.out.println(3*3);
+		System.out.println(3*4);
+		System.out.println(3*5);
+		System.out.println(3*6);
+		System.out.println(3*7);
+		System.out.println(3*8);
+		System.out.println(3*9);
+		// 라인 복사 맥 단축키 cmd+option+아래
+		
+		
+		
+	}
+
+}
